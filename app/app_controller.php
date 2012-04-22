bc434459b507707b928058041d96f43313e91a66
deep manandhar <dkm351@gmail.com>
2012-04-21 22:56:00 -0400

    deep changes

--- app/app_controller.php
+++ app/app_controller.php
@@ -1,6 +1,7 @@
 <?php
 class AppController extends Controller{
 	var $components = array('Auth','Session');
+	
 	var $helpers = array('Js','Session');
 	var $_User = array(); 
 	function beforeFilter(){
